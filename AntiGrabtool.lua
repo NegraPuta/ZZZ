806f4f0d2aa7cf29c5d111045b250018ff07c59e
Gringoszzzzzzzz <108856608+NegraPuta@users.noreply.github.com>
2022-08-13 00:36:50 -0400

    Add files via upload

---
+++ AntiGrabtool.lua
@@ -0,0 +1,17 @@
+local Stepped = game:GetService("RunService").Stepped:Connect(
+    function()
+        local Part,distance = CFrame.new(-265.755157, 279.427368, 1000.90588, -0.0678123757, 5.10859834e-08, -0.997698069, 3.46426288e-09, 1, 5.09683886e-08, 0.997698069, -8.63215088e-16, -0.0678123757)
+        if Player.Character then
+            local busco = Player.Character:FindFirstChild("UpperTorso")
+            if busco then
+                distance = (Players.Character.UpperTorso.Position - Part.Position).Magnitude
+                local Studs = (math.floor(distance))
+                print(Studs)
+                if Studs >= 0.5 then
+                    ServerHop()
+                    Stepped:Disconnect()
+                end
+            end
+        end
+    end
+)
