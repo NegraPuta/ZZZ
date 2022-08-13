d7991413dec10edea401985b0f0fa189ff7378f8
Gringoszzzzzzzz <108856608+NegraPuta@users.noreply.github.com>
2022-08-13 02:14:12 -0400

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,6 +1,6 @@
 game.Players.LocalPlayer.Character.Humanoid:UnequipTools()
 game.ReplicatedStorage.RemoteEvent:FireServer({"Skill_Invisible", "Start"})
-for i, v in pairs(getconnections(Player.PlayerGui.ScreenGui.MenuBtn.MouseButton1Click)) do
+for i, v in pairs(getconnections(game.Players.LocalPlayer.PlayerGui.ScreenGui.MenuBtn.MouseButton1Click)) do
     v.Function()
 end
 for i, v in pairs(getconnections(game.Players.LocalPlayer.Idled)) do
