6ac5cb3e5076897b684fee5e3385cc065b557c76
Gringoszzzzzzzz <108856608+NegraPuta@users.noreply.github.com>
2022-08-13 02:11:49 -0400

    Update AntiGhostRiderKill.lua

--- AntiGhostRiderKill.lua
+++ AntiGhostRiderKill.lua
@@ -1,5 +1,5 @@
 local Stepped = game:GetService("RunService").Stepped:Connect(function()
-    if Player.Character.Humanoid.Health == 0 then
+    if game.Players.LocalPlayer.Character.Humanoid.Health == 0 then
         ServerHop()
         Stepped:Disconnect()
     end
