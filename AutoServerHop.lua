b0aa89bbe9835088df0cb5bba58baf1e3242b8eb
Gringoszzzzzzzz <108856608+NegraPuta@users.noreply.github.com>
2022-08-13 16:14:43 -0400

    Update AutoServerHop.lua

--- AutoServerHop.lua
+++ AutoServerHop.lua
@@ -1,95 +1,16 @@
---Not made by me
-if syn.queue_on_teleport then
-    syn.queue_on_teleport('game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()')
-end
-local PlaceID = game.PlaceId
-local AllIDs = {}
-local foundAnything = ""
-local actualHour = os.date("!*t").hour
-local Deleted = false
-local File =
-    pcall(
-    function()
-        AllIDs = game:GetService("HttpService"):JSONDecode(readfile("NotSameServers.json"))
-    end
-)
-if not File then
-    table.insert(AllIDs, actualHour)
-    writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(AllIDs))
-end
-function TPReturner()
-    local Site
-    if foundAnything == "" then
-        Site =
-            game.HttpService:JSONDecode(
-            game:HttpGet("https://games.roblox.com/v1/games/" .. PlaceID .. "/servers/Public?sortOrder=Asc&limit=100")
-        )
-    else
-        Site =
-            game.HttpService:JSONDecode(
-            game:HttpGet(
-                "https://games.roblox.com/v1/games/" ..
-                    PlaceID .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. foundAnything
-            )
-        )
-    end
-    local ID = ""
-    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-        foundAnything = Site.nextPageCursor
-    end
-    local num = 0
-    for i, v in pairs(Site.data) do
-        local Possible = true
-        ID = tostring(v.id)
-        if tonumber(v.maxPlayers) > tonumber(v.playing) then
-            for _, Existing in pairs(AllIDs) do
-                if num ~= 0 then
-                    if ID == tostring(Existing) then
-                        Possible = false
-                    end
-                else
-                    if tonumber(actualHour) ~= tonumber(Existing) then
-                        local delFile =
-                            pcall(
-                            function()
-                                delfile("NotSameServers.json")
-                                AllIDs = {}
-                                table.insert(AllIDs, actualHour)
-                            end
-                        )
-                    end
-                end
-                num = num + 1
-            end
-            if Possible == true then
-                table.insert(AllIDs, ID)
-                wait()
-                pcall(
-                    function()
-                        writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(AllIDs))
-                        wait()
-                        game:GetService("TeleportService"):TeleportToPlaceInstance(
-                            PlaceID,
-                            ID,
-                            game.Players.LocalPlayer
-                        )
-                    end
-                )
-                wait(4)
-            end
+local function ServerHop()
+local GUIDs = {}
+local Http = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100&cursor="))
+for i = 1,5 do
+    for i,v in pairs(Http.data) do
+        if v.playing ~= v.maxPlayers and v.id ~= game.JobId then
+            maxPlayers = v.maxPlayers
+            table.insert(GUIDs, {id = v.id, users = v.playing})
         end
     end
+    print("-","Page searched...")
+    if Http.nextPageCursor ~= null then Http = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100&cursor="..Http.nextPageCursor)) else break end
 end
-
-local function ServerHop()
-    while wait(1) do
-        pcall(
-            function()
-                TPReturner()
-                if foundAnything ~= "" then
-                    TPReturner()
-                end
-            end
-        )
-    end
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, GUIDs[math.random(1,#GUIDs)].id)
+wait(2)
 end
