1af923df64d22c4e0305963c1c4e5ebd40f8ef15
Gringoszzzzzzzz <108856608+NegraPuta@users.noreply.github.com>
2022-08-14 21:38:36 -0400

    Delete AntiGhostRiderKill.lua

--- AntiGhostRiderKill.lua
+++
@@ -1,7 +0,0 @@
-local stepped = game:GetService("RunService").Stepped:Connect(function()
-        if Player.Character.Humanoid.Health == 0 then
-            ServerHop()
-            stepped:Disconnect()
-        end
-    end
-)
